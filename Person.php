c623979579781c2caf6a71f669ee569765986417
Ossi <karja.ossi@gmail.com>
2019-03-17 18:34:23 +0200

    Create Person.php

---
+++ Person.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ *
+ */
+abstract class Person
+{
+
+  private $fname;
+  private $lname;
+  private $yobirth;
+
+  public function setfirstName($value)
+  {
+    $this->$fname=$value;
+  }
+  public function setlastName($value)
+  {
+    $this->$lname=$value;
+  }
+  public function setyearOfBirth($value)
+  {
+    $this->$yobirth=$value;
+  }
+  public function getfirstName()
+  {
+    return $this->$fname;
+  }
+  public function getlastName()
+  {
+    return $this->$lname;
+  }
+  public function getyearOfBirth()
+  {
+    return $this->$yobirth;
+  }
+}
+
+
+ ?>
